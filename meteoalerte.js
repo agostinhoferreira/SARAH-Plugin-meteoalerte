d567ea637a3fe1c50629377dde7bfe239eb4859c
Ferreira77 <maytetino@gmail.com>
2013-11-17 23:33:04 +0100

    Sarah Alerte meteo france version 1.1
    Alerte meteo

--- meteoalerte.js
+++ meteoalerte.js
@@ -7,23 +7,28 @@ exports.action = function(data, callback, config, SARAH){
 					return;
 					}
 		console.log('dept :'+config.numero_departement);
-		//---------------------------------*******************------------------------------------------
-		var url = 'http://france.meteofrance.com/vigilance/Bulletin?ZONE=DEPT'+config.numero_departement;
+		/*
+		// Le site de meteo france 
+		// Ne donne pas la couleur du departement quand supérieur a jaune
+		// var url = 'http://france.meteofrance.com/vigilance/Bulletin?ZONE=DEPT'+config.numero_departement;
+		//
+		*/
+		var url = 'http://meteo.mytinn.fr/sarah_al_meteo.php?dept='+config.numero_departement;
 		var request = require('request');
 		request({ 'uri' : url }, function (err, response, body){
-				//debug
-				//console.log(url);
 				if (err || response.statusCode != 200) {
 												callback({'tts': "Le site Meteo n'est pas disponible"});
 												return;
 												}
+				/*
 				var $ = require('cheerio').load(body, { xmlMode: false });
 				var vigilance=$("body .texte12_bull span").text(); // class TD
 				var crue=$("body .verda11 span").text(); // class TD
-				// debug
-				//console.log(vigilance);
-				//console.log(crue);
-				callback({'tts' : 'Pour le '+ config.numero_departement +' le '+ vigilance+ ' ' + crue });
+				 debug
+				console.log(vigilance);
+				console.log(crue);
+				*/
+				callback({'tts' : body });
 				// version 1.2
 				
 		});
